29cc5a32c4efa7a17070c38f2e02bad7854315bd
Eddy Yoel Fresno Hdz <fresnohernandez99@gmail.com>
2023-01-31 22:50:18 -0500

    created menu and reset functionality

---
+++ scenes/gui/Menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+onready var menuBtn = $MenuButton
+var popup
+
+func _ready():
+	popup = menuBtn.get_popup()
+	popup.add_item("Reset")
+	popup.add_item("Options")
+	popup.add_item("Quit")
+	popup.connect("id_pressed", self, "_on_item_pressed")
+
+func _on_item_pressed(ID):
+	match ID:
+		0:
+			goToStart()
+		1:
+			goToSettings()
+		2:
+			exitGame()
+
+func goToStart():
+	get_tree().change_scene("res://scenes/gui/MainMenu.tscn")
+
+func goToSettings():
+	pass
+
+func exitGame():
+	get_tree().quit()
