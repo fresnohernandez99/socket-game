3cb17e9295419facffa05ec63a1844655bd78092
Eddy Yoel Fresno Hdz <fresnohernandez99@gmail.com>
2023-02-04 10:47:33 -0500

    added initial animation 2

--- scenes/ui/Start.gd
+++ scenes/ui/Start.gd
@@ -2,9 +2,12 @@ extends Node
 
 
 onready var story1 = $StoryLv1
+onready var story2 = $StoryLv2
+onready var story2Sprite = $StoryLv2/Sprite
 onready var label = $ScrollContainer/MessageLabel
 onready var tween = $Tween
 onready var finalPos = $Position2D
+onready var nextBtn = $NextBtn
 
 onready var images = [$Hercules, $Ochosi, $Susanoo, $Templar, $Veles]
 
@@ -23,34 +26,50 @@ var texts = [
 ]
 
 func _ready():
-	showFade(story1)
+	showFade(story1, 5.0)
 	label.bbcode_text = texts[cont]
 
-func showFade(obj: Sprite):
-	tween.interpolate_property(obj, "modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1), 5.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+func showFade(obj, time):
+	tween.interpolate_property(obj, "modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1), time, Tween.TRANS_LINEAR, Tween.EASE_IN)
 
-func showFadeAndMove(obj: Sprite):
+func hideFade(obj):
+	tween.interpolate_property(obj, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 3.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+
+
+func showFadeAndMove(obj):
 	tween.interpolate_property(obj, "position", obj.position, finalPos.position, 4.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
 
 
 func _on_NextBtn_pressed():
+	nextBtn.disabled = true
 	if cont == 1:
 		tween.start()
 
 	if cont == 4:
 		startTransition()
+		
+	if cont == 6:
+		hideFade(story1)
+		tween.start()
+		showFade(story2, 3.0)
+		story2Sprite.play()
+		tween.start()
+		story2.visible = true
 
 	if cont < texts.size() -1:
 		cont += 1
 		label.bbcode_text = texts[cont]
 	else:
 		pass
+	
+	yield(get_tree().create_timer(3), "timeout")
+	nextBtn.disabled = false
 
 func startTransition():
 	var cont2 = 0
 	while cont2 < images.size():
 		yield(get_tree().create_timer(1), "timeout")
 		showFadeAndMove(images[cont2])
-		showFade(images[cont2])
+		showFade(images[cont2], 5.0)
 		tween.start()
 		cont2 +=1
